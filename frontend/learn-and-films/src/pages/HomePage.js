8a7a58977fe8ce59291a2452bf7d943440857e61
Moctar93 <moctartize93@gmail.com>
2024-10-26 00:05:47 +0200

    update file App.js and add another files for frontend

---
+++ frontend/learn-and-films/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+	 <h1>Bienvenue sur la home page</h1>
+      {/* Autres éléments de la page */}
+    </div>
+  );
+}
+
+export default HomePage;
+
